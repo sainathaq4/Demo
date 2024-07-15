b05f13e6a773ce412439e4b9cbea12ff377bf9a3
sainath <sainath@analyticsquad4.com>
2024-07-15 20:12:15 +0530

    first commit

---
+++ src/shaders/vertex.glsl
@@ -0,0 +1,22 @@
+uniform float uTime;
+
+varying vec2 vUv;
+varying float vElevation;
+
+void main()
+{
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    float elevation = sin(modelPosition.x  - uTime) * 0.1;
+    elevation += sin(modelPosition.y  - uTime) * 0.1;
+
+    modelPosition.z += elevation;
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectedPosition;
+
+    vUv = uv;
+    vElevation = elevation;
+}
